5a9ef8d35931f5f383c9cd22b1990572b4691b21
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-03-28 20:48:46 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-secondary-500 text-display1">Hello World</h1>
+    </div>
+  );
+}
